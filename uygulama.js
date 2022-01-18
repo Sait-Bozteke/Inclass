855fe42db7b392f5f3a6e13437c989cf44f6b69e
sait <sait.bozteke63@gmail.com>
2022-01-18 14:13:48 +0100

    file added

---
+++ uygulama.js
@@ -0,0 +1,72 @@
+console.log("hello world");
+console.log(8);
+var isim = "sait";
+console.log(isim);
+let fullname = "sait bozteke";
+console.log(fullname);
+const fullname1 = "tugba bozteke";
+console.log(fullname1);
+const email = "sait.bozteke@gmail.com";
+console.log(email);
+
+var order1 = Number("100");
+var order2 = 150;
+var total_order = order1 + order2;
+
+console.log(total_order);
+
+console.log(typeof order1);
+var b = null;
+console.log(b);
+var isim = "sait";
+console.log(typeof isim);
+var age = 34;
+console.log(typeof age);
+var online = true;
+console.log(typeof online);
+
+var cars = ["mercedes", "toyota", "audi"];
+console.log(typeof cars);
+
+var person = {
+  isim1: "tugba",
+  isim2: "sait",
+};
+console.log(typeof person);
+var isalive = function () {
+  return 0;
+};
+console.log(typeof isalive);
+
+var sonuc;
+const x = 60;
+const y = 12;
+let z = 7;
+let t = "9";
+
+sonuc = x + y;
+sonuc = x - y;
+sonuc = x * y;
+sonuc = x / y;
+sonuc = x % z;
+sonuc = x % y;
+sonuc = x ** y;
+sonuc = z++;
+sonuc = ++z;
+
+sonuc += x;
+sonuc -= x;
+sonuc *= x;
+sonuc /= x;
+
+sonuc = x == y;
+sonuc = x != y;
+
+z = 9;
+t = "9";
+sonuc = z == t;
+
+sonuc = !(x > z);
+
+console.log(sonuc);
+console.log(typeof sonuc);
