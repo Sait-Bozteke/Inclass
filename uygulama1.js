603fdef5a4f6e03b0ea71a3525ba4cb4a09da35c
sait <sait.bozteke63@gmail.com>
2022-01-18 14:53:15 +0100

    file added

---
+++ uygulama1.js
@@ -0,0 +1,22 @@
+const ad = "sait";
+const soyad="bozteke";
+var x=ad+" "+soyad;
+
+x=ad.concat(" "+ soyad);
+x=ad.length
+x= ad.toUpperCase;
+x= ad.toLowerCase;
+x= ad.indexOf("t");
+x= soyad.indexOf("z")
+x= ad.substring(0,2);
+x = ad.slice(0,3);
+x= ad.replace("sait", "tugba");
+
+
+
+
+
+
+
+
+console.log(x);
