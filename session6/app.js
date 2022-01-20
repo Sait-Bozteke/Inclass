9ee0b73a36a4db94840a5a0eac6ff7d56301af5c
sait <sait.bozteke63@gmail.com>
2022-01-20 21:29:21 +0100

    file added

---
+++ session6/app.js
@@ -0,0 +1,35 @@
+// console.log("hello world");
+// var x=7;
+// var y=5;
+// console.log({x});
+// console.log({y});
+// y=x
+// console.log({y});
+// var a="global"
+// console.log(a);
+// {
+// let a="scope"
+// console.log(a);
+// {
+// let a="inner scope"
+// console.log(a);}
+
+
+// console.log(a);
+// let c=3;
+// console.log(c);
+//  c=5;
+// console.log(c);
+// var a=5;
+// const h="constant variale";
+// console.log(h);
+// console.log(d);
+// var d=6;
+
+
+// var f=;
+// console.log(typeof f);
+// f=5;
+// console.log(typeof f);
+
+console.log(typeof );
